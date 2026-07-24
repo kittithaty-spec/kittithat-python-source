a8bbb0461a3a4d2cc4c3eac3c5badbf4a207e875
kittithat yamoubol <kittithat.y@ku.th>
2026-07-24 12:18:39 +0700

    loop

--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -8,9 +8,20 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
-
-
-
+if age >= 90:
+    print("very old")
+elif age >= 80:
+    print("old")
+elif age >= 60:
+    print("Almost old")
+elif age >= 40:
+    print("Working age")
+elif age >= 20:
+    print("Still Young")
+elif age >= 10:
+    print("Young")
+else:
+    print("Kid")
 # Complete this ATM simulation
 balance = 1000
 pin = "1234"
@@ -28,6 +39,23 @@ if entered_pin == pin:
         
         # Complete the menu logic here
         # Your code here:
-        
+        if choice == "1":
+            print(f"Your balance: {balance}")
+        elif choice == "2":
+            How_much = float(input("Enter amount: "))
+            if How_much > balance:
+                print("No try again")
+            elif How_much <= balance:
+                balance = balance - How_much
+                print(f"balance now: {balance}")
+        elif choice == "3":
+            How_muchAdd = float(input("Enter amount: "))
+            if How_muchAdd <= 0:
+                print("No")
+            elif How_muchAdd >0:        
+             balance = balance + How_muchAdd
+             print(f"balance now: {balance}")
+        elif choice == "4":
+            break            
 else:
     print("Invalid PIN")
