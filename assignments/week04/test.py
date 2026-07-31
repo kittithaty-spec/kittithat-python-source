87e8915faceb84de92442a4db314c317b2a3805b
kittithat yamoubol <kittithat.y@ku.th>
2026-07-31 12:06:19 +0700

    aeiou

---
+++ assignments/week04/test.py
@@ -0,0 +1,11 @@
+# รับชื่อจริงจากผู้ใช้
+# นับจำนวนสระทั้งหมดในข้อความว่ามีกี่ตัว (a, e, i, o, u)
+# โดยใช้ loop-for เท่านั้น
+
+Name = input("Enter your Name: ").lower()
+
+vowels = 'aeiouAEIOU'
+
+count = sum(1 for char in Name if char in vowels)
+
+print(f"สระที่มี: {count}")
